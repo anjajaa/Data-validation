aa7a00bf06f7831931c3fef7beb09bb60839f2e7
anjajaa <87967500+anjajaa@users.noreply.github.com>
2021-07-26 09:59:44 +0200

    Update loop_ar_status.sh

--- loop_ar_status.sh
+++ loop_ar_status.sh
@@ -2,7 +2,9 @@
 
 #change value of one column based on the values in other two and the highest number in one.
 
+#in case there was already an array with the same name created, good to remove rules assigned to it
 unset version_array;
+#create an asociative array
 declare -A version_array;
 
 function max_version ()
